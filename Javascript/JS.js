d9680cd9f804b9a06d7adb38dbd7c0db62a56948
LRHCS <3101847378@qq.com>
2021-12-27 19:08:10 +0100

    Add a tech blog page image and text shadow

---
+++ Javascript/JS.js
@@ -0,0 +1,12 @@
+//alert("Welcome to the Website from Rui");
+
+// Change heading:
+document.getElementById("myH").innerHTML = "My First Page";
+
+
+// Change heading:
+document.getElementById("myH").innerHTML = "JavaScript Comments";
+// Change paragraph:
+
+
+
