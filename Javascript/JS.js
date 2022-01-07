302ff3b0f03b97cf5ecc49ee5d9457f74513ab9e
LRHCS <3101847378@qq.com>
2022-01-07 21:38:29 +0100

    Fix the Bug of the Nav bar

--- Javascript/JS.js
+++ Javascript/JS.js
@@ -1,3 +1,10 @@
+//jQuery
+src="https://code.jquery.com/jquery-3.6.0.slim.js"
+        integrity="sha384-fuUlMletgG/KCb0NwIZTW6aMv/YBbXe0Wt71nwLRreZZpesG/N/aURjEZCG6mtYn"
+        crossOrigin="anonymous"
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+
+
 //alert("Welcome to the Website from Rui");
 
 // Change heading:
@@ -7,7 +14,17 @@ document.getElementById("myH").innerHTML = "My First Page";
 // Change heading:
 document.getElementById("myH").innerHTML = "JavaScript Comments";
 
+//jQuery
 
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+    });
+});
 
+$(document).ready(function(){
+    $("button").click(function(){
+        $(".test").slideToggle(300);
 
-
+    });
+});
